9bb8699079b97de3d61c14f01571e14f52a3d293
Sui Yee <yeeng2001@gmail.com>
2024-01-09 09:57:04 +0800

    update env

--- lawfirm/src/firebase.js
+++ lawfirm/src/firebase.js
@@ -8,14 +8,14 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDQ1sbcukGiRdmoE7nRDfy_vMyuYYXIIS0",
-  authDomain: "cmt322-7a0f1.firebaseapp.com",
-  databaseURL: "https://cmt322-7a0f1-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "cmt322-7a0f1",
-  storageBucket: "cmt322-7a0f1.appspot.com",
-  messagingSenderId: "840356715981",
-  appId: "1:840356715981:web:6951b405de14e0416a7884",
-  measurementId: "G-LTD06E7S9D"
+  apiKey:  `${process.env.REACT_APP_API_KEY}`,
+  authDomain:  `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  databaseURL: `${process.env.REACT_DATABASE_URL}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`,
+  measurementId: `${process.env.REACT_APP_MEASUREMENT_ID}`,
 };
 
 // Initialize Firebase
