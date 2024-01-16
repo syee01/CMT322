3146a4ef79253ba708ebfdced597b5a80285a795
mohlingzheng <mohlingzheng@gmail.com>
2024-01-16 17:46:21 +0800

    add view tab for each meeting

--- lawfirm/src/googleapi.js
+++ lawfirm/src/googleapi.js
@@ -2,7 +2,7 @@ import ApiCalendar from 'react-google-calendar-api';
   
 const config = {
     "clientId": '1066631153230-6bajraku2jr8eu5ankrab26td36shqi1.apps.googleusercontent.com',
-    "apiKey": 'AIzaSyDMyzez8LTmAPDAMCN4-j1p3nL0HLGidrU',
+    "apiKey": 'AIzaSyAqvNY2pJY64vsXyyMGesJqqH8sN9oZxY8',
     "scope": "https://www.googleapis.com/auth/calendar",
     "discoveryDocs": [
         "https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"
